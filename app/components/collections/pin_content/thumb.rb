47d4ba04fa254b23b89a65407a9da4e3caec37ef
mbajur <mbajur@gmail.com>
2026-06-13 20:54:26 +0200

    Fix collection description formatting in collection pin

--- app/components/collections/pin_content/thumb.rb
+++ app/components/collections/pin_content/thumb.rb
@@ -1,10 +1,12 @@
 module Components
   module Collections
     class PinContent::Thumb < Pins::Pin::ThumbBase
+      include Phlex::Rails::Helpers::SimpleFormat
+
       def view_template(&)
         container do
           div do
-            Text(size: "2", class: "mt-2") { @pin.pinable.description }
+            simple_format(pinable_description, class: "text-sm font-normal mt-2") if pinable_description.present?
             Components::Collections::MetaInfo(collection: @pin.pinable, opts: { show_author: true })
           end
         end
@@ -14,6 +16,10 @@ module Components
 
       def core_container_classes
         super + " p-3"
+      end
+
+      def pinable_description
+        @pin.pinable.description
       end
     end
   end
