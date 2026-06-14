4d441758b92de7493ed8b5ba7f8d7f817fbbf083
mbajur <mbajur@gmail.com>
2026-06-14 16:12:53 +0200

    Switch to polynomially_longer for ProcessScreenshotHandlerJob

--- app/jobs/url_thumbnailer/process_screenshot_handler_job.rb
+++ app/jobs/url_thumbnailer/process_screenshot_handler_job.rb
@@ -2,7 +2,7 @@ class UrlThumbnailer::ProcessScreenshotHandlerJob < ApplicationJob
   queue_as :screenshoter
   limits_concurrency to: 1, key: ->(*) { "screenshoter" }
 
-  retry_on Ferrum::DeadBrowserError, wait: :exponentially_longer, attempts: 5
+  retry_on Ferrum::DeadBrowserError, wait: :polynomially_longer, attempts: 5
 
   def perform(post, handler_class:)
     handler_class.new(post: post, preflight: nil).process_screenshot
