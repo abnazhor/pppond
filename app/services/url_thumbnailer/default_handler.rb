0683eaf8f8dfc523fad5115ef54fae73ab16cf75
mbajur <mbajur@gmail.com>
2026-07-01 10:46:17 +0200

    Save post after attaching a thumb in default handler

--- app/services/url_thumbnailer/default_handler.rb
+++ app/services/url_thumbnailer/default_handler.rb
@@ -39,6 +39,7 @@ class UrlThumbnailer::DefaultHandler
 
       logger.info "Attaching thumbnail to URL cache for URL: #{@post.url}..."
       @post.thumb.attach(process_image(tmp))
+      @post.save!
     rescue Faraday::ResourceNotFound, Faraday::ForbiddenError => e
       # Do not report that anywhere, there will be a lot of cases like this and we can't do much about that.
       logger.warn "Thumbnail not found at URL: #{image_url} for post URL: #{@post.url}. Error: #{e.message}"
